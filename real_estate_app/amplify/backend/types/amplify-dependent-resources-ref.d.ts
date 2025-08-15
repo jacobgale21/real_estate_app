9540f082b064595cbbd234a3570b36bb567ae783
JacobGale <jacob.gale@ufl.edu>
2025-08-15 00:10:01 -0400

    Added user authentication through AWS Amplify

---
+++ real_estate_app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "realestateapp0047c6af": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
